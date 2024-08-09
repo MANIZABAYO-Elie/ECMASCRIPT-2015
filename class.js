19e9d0c8baad70d0fffaf3cd1e603e30b5e93525
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-08-09 17:02:19 +0200

    file commited

--- class.js
+++ class.js
@@ -14,44 +14,7 @@ const areaOfRectangle = new rectangle(20,40)
 console.log(areaOfRectangle.area);
 
 // 
-class Car {
-    constructor(name) {
-      this.brand = name;
-    }
+class polygon {
+    
+}
   
-    present() {
-      return 'I have a ' + this.brand;
-    }
-  }
-  //
-  class Car {
-    constructor(name) {
-      this.brand = name;
-    }
-  
-    present() {
-      return 'I have a ' + this.brand;
-    }
-  }
-  
-  class Car {
-    constructor(name) {
-      this.brand = name;
-    }
-  
-    present() {
-      return 'I have a ' + this.brand;
-    }
-  }
- 
-  class Model extends Car {
-    constructor(name, mod) {
-      super(name);
-      this.model = mod;
-    }  
-    show() {
-        return this.present() + ', it is a ' + this.model
-    }
-  }
-  const mycar = new Model("Ford", "Mustang");
-  mycar.show();
