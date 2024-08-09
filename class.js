a0b8d3e49b193afcd1c628f79cd4a2f388e224c3
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-08-09 11:49:20 +0200

    first commit

--- class.js
+++ class.js
@@ -3,4 +3,12 @@ class rectangle  {
        this.height = height 
        this.width = width 
     }
+    get area(){
+        return this.calArea()
+    }
+    calArea(){
+        return this.height*this.width 
+    }
 }
+const areaOfRectangle = new rectangle(20,40)
+console.log(areaOfRectangle.area);
