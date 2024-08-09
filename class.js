c0e51c51fa9645fbddcfa5b2cff4f0a014e1a2f1
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-08-09 12:38:44 +0200

    file committed

--- class.js
+++ class.js
@@ -14,13 +14,44 @@ const areaOfRectangle = new rectangle(20,40)
 console.log(areaOfRectangle.area);
 
 // 
-class name {
+class Car {
     constructor(name) {
-    this.name = name
-        }
-    speak(){
-        return `${this.name} backs`
+      this.brand = name;
     }
-}
-const dogSound = new name("dog")
-console.log(dogSound);
+  
+    present() {
+      return 'I have a ' + this.brand;
+    }
+  }
+  //
+  class Car {
+    constructor(name) {
+      this.brand = name;
+    }
+  
+    present() {
+      return 'I have a ' + this.brand;
+    }
+  }
+  
+  class Car {
+    constructor(name) {
+      this.brand = name;
+    }
+  
+    present() {
+      return 'I have a ' + this.brand;
+    }
+  }
+ 
+  class Model extends Car {
+    constructor(name, mod) {
+      super(name);
+      this.model = mod;
+    }  
+    show() {
+        return this.present() + ', it is a ' + this.model
+    }
+  }
+  const mycar = new Model("Ford", "Mustang");
+  mycar.show();
